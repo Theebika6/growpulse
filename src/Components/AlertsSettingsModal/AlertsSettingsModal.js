9636377b0127ed57d6d1aeec752ddadebc965644
Patrickriachi <patrickriachi123@gmail.com>
2024-01-22 10:04:05 -0500

    settings button for thresholds

--- src/Components/AlertsSettingsModal/AlertsSettingsModal.js
+++ src/Components/AlertsSettingsModal/AlertsSettingsModal.js
@@ -29,6 +29,40 @@ const AlertsSettingsModal = ({ systemName, onClose }) => {
         fetchHumidityOffset(setHumidityOffset, systemName);
     }, [systemName]);
 
+    const handleChange = (currentValue, operation, setValue, limitMin, limitMax) => {
+        let newValue = parseFloat(currentValue);
+
+        if (operation === 'increment') {
+            newValue = newValue + 1;
+            if (newValue > limitMax) {
+                newValue = limitMax;
+            }
+        } else if (operation === 'decrement') {
+            newValue = newValue - 1;
+            if (newValue < limitMin) {
+                newValue = limitMin;
+            }
+        }
+        setValue(newValue.toString());
+    };
+
+    const handleChangeTDS = (currentValue, operation, setValue, limitMin, limitMax) => {
+        let newValue = parseFloat(currentValue);
+
+        if (operation === 'increment') {
+            newValue = newValue + 10;
+            if (newValue > limitMax) {
+                newValue = limitMax;
+            }
+        } else if (operation === 'decrement') {
+            newValue = newValue - 10;
+            if (newValue < limitMin) {
+                newValue = limitMin;
+            }
+        }
+        setValue(newValue.toString());
+    };
+
     return (
         <div className="alerts-modal-container">
             <div className="alerts-modal">
@@ -38,8 +72,18 @@ const AlertsSettingsModal = ({ systemName, onClose }) => {
                     <tr>
                         <td>pH</td>
                         <td>
-                            Min: <input type="number" value={phMin} onChange={(e) => setPhMin(e.target.value)} />
-                            Max: <input type="number" value={phMax} onChange={(e) => setPhMax(e.target.value)} />
+                            Min:
+                            <div className="input-container">
+                                <button onClick={() => handleChange(phMin, 'decrement', setPhMin, 0, phMax)}> - </button>
+                                <input type="number" value={phMin} onChange={(e) => setPhMin(e.target.value)} />
+                                <button onClick={() => handleChange(phMin, 'increment', setPhMin, 0, phMax)}> + </button>
+                            </div>
+                            Max:
+                            <div className="input-container">
+                                <button onClick={() => handleChange(phMax, 'decrement', setPhMax, phMin, 14)}> - </button>
+                                <input type="number" value={phMax} onChange={(e) => setPhMax(e.target.value)} />
+                                <button onClick={() => handleChange(phMax, 'increment', setPhMax, phMin, 14)}> + </button>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -52,14 +96,24 @@ const AlertsSettingsModal = ({ systemName, onClose }) => {
                     <tr>
                         <td>Water Temp</td>
                         <td>
-                            Min: <input type="number" value={waterTempMin} onChange={(e) => setWaterTempMin(e.target.value)} />
-                            Max: <input type="number" value={waterTempMax} onChange={(e) => setWaterTempMax(e.target.value)} />
+                            Min:
+                            <div className="input-container">
+                                <button onClick={() => handleChange(waterTempMin, 'decrement', setWaterTempMin, 0, waterTempMax)}> - </button>
+                                <input type="number" value={waterTempMin} onChange={(e) => setWaterTempMin(e.target.value)} />
+                                <button onClick={() => handleChange(waterTempMin, 'increment', setWaterTempMin, 0, waterTempMax)}> + </button>
+                            </div>
+                            Max:
+                            <div className="input-container">
+                                <button onClick={() => handleChange(waterTempMax, 'decrement', setWaterTempMax, waterTempMin, 24)}> - </button>
+                                <input type="number" value={waterTempMax} onChange={(e) => setWaterTempMax(e.target.value)} />
+                                <button onClick={() => handleChange(waterTempMax, 'increment', setWaterTempMax, waterTempMin, 24)}> + </button>
+                            </div>
                         </td>
                     </tr>
                     <tr>
                         <td>Humidity</td>
-                        <td>
-                            Offset:
+                        <td className="slider-text">
+                            <span>Offset:</span>
                             <input
                                 type="range"
                                 min="5"
@@ -73,8 +127,18 @@ const AlertsSettingsModal = ({ systemName, onClose }) => {
                     <tr>
                         <td>Air Temp</td>
                         <td>
-                            Min: <input type="number" value={airTempMin} onChange={(e) => setAirTempMin(e.target.value)} />
-                            Max: <input type="number" value={airTempMax} onChange={(e) => setAirTempMax(e.target.value)} />
+                            Min:
+                            <div className="input-container">
+                                <button onClick={() => handleChange(airTempMin, 'decrement', setAirTempMin, 0, airTempMax)}> - </button>
+                                <input type="number" value={airTempMin} onChange={(e) => setAirTempMin(e.target.value)} />
+                                <button onClick={() => handleChange(airTempMin, 'increment', setAirTempMin, 0, airTempMax)}> + </button>
+                            </div>
+                            Max:
+                            <div className="input-container">
+                                <button onClick={() => handleChange(airTempMax, 'decrement', setAirTempMax, airTempMin, 30)}> - </button>
+                                <input type="number" value={airTempMax} onChange={(e) => setAirTempMax(e.target.value)} />
+                                <button onClick={() => handleChange(airTempMax, 'increment', setAirTempMax, airTempMin, 30)}> + </button>
+                            </div>
                         </td>
                     </tr>
                     </tbody>
