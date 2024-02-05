0e00b49b525b8b20e0c2a7351dd95cd3279a643b
Patrickriachi <patrickriachi123@gmail.com>
2024-02-05 15:53:42 -0500

    fixes

--- src/Components/Services/CameraServices.js
+++ src/Components/Services/CameraServices.js
@@ -5,7 +5,7 @@ import { storage } from '../../firebaseConfig';
 export const fetchImage = async (systemName) => {
     const currentUser = auth.currentUser;
     if (currentUser) {
-        const storageRef = ref(storage, `Registered Users/${currentUser.uid}/${systemName}/placeHolder.jpg`);
+        const storageRef = ref(storage, `Registered Users/${currentUser.uid}/${systemName}/placeHolder.jpeg`);
         try {
             const url = await getDownloadURL(storageRef);
             return url;
